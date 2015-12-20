4d449e0d9dd2bed76779bc3afff2aec3122eea73
jiamianshiye <jiamianshiye@jiamianshiye.(none)>
2015-12-20 22:35:00 +0800

    add hannoi test

---
+++ hanoi.h
@@ -0,0 +1,5 @@
+#ifndef HANOI
+#define HANOI
+
+#endif // HANOI
+
--- main.c
+++ main.c
@@ -5,14 +5,25 @@
 
 #include "maze.h"
 
+#include "hanoi.h"
+
+#include <time.h>
+#include <sys/time.h>
 struct StackObjArr stkobj;
 int main(int argc, char *argv[])
 {
-
+#if 0
+    struct timeval vp, va;
+    gettimeofday(&vp, NULL);
+    hanoi();
+    gettimeofday(&va, NULL);
+    printf("Cost of time : %d ms\n", (va.tv_sec - vp.tv_sec)*1000 + (va.tv_usec - vp.tv_usec)/1000);
+#endif
+#if 0
     stkInit(&stkobj, 128);
 
     maze_work(&stkobj);
-#if 0
+
     stkInit(&stkobj, 3);
     char msg1[10]="hello";
     char msg2[10]="world";
